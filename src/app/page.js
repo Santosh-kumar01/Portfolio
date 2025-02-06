211a54fed3bb002514694347a2fa2cbdc12b5a7b
Santosh kumar <bk318710@gmail.com>
2025-02-06 13:59:07 +0530

    add locomotive-scroll for smooth scrolling effect and update page structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,52 @@
+"use client";
+import dynamic from "next/dynamic";
 import About from "@/components/About/About";
 import Contact from "@/components/Contact/Contact";
-
 import Home from "@/components/Home/Home";
 import Portfolio from "@/components/Portfolio/Portfolio";
-import Services from "@/components/Testimonial/Testimonial";
-
+import Testimonial from "@/components/Testimonial/Testimonial";
 import Skills from "@/components/Skills/Skills";
 import Footer from "@/components/Footer/Footer";
+import React, { useEffect, useRef, useState } from "react";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
-import React from "react";
+// Dynamically import LocomotiveScroll only in the browser
+const LocomotiveScroll = dynamic(() => import("locomotive-scroll"), {
+  ssr: false,
+});
 
+const Page = () => {
+  const scrollContainerRef = useRef(null);
+  const [locomotiveScroll, setLocomotiveScroll] = useState(null);
 
+  useEffect(() => {
+    if (typeof window !== "undefined" && scrollContainerRef.current) {
+      import("locomotive-scroll").then((module) => {
+        const LocomotiveScrollInstance = new module.default({
+          el: scrollContainerRef.current,
+          smooth: true,
+        });
 
-const page = () => {
+        setLocomotiveScroll(LocomotiveScrollInstance);
+      });
+
+      return () => {
+        if (locomotiveScroll) locomotiveScroll.destroy();
+      };
+    }
+  }, []);
+
   return (
-
-      <div className=" w-full bg-violet-50">
-        <Home />
-        <About />
-        <Skills />
-        <Services />
-        <Portfolio />
-        <Contact />
-        <Footer />
-      </div>
-
-
+    <div ref={scrollContainerRef} className="w-full bg-violet-50">
+      <Home />
+      <About />
+      <Skills />
+      <Testimonial />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
-export default page;
+export default Page;
